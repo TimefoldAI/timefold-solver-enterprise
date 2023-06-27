21a1f3a94a628f71648318319847bcdf3ed3a443
Lukáš Petrovický <lukas@petrovicky.net>
2023-06-27 08:02:45 +0200

    ci: minor tweaks

--- .github/scripts/change_versions.sh
+++ .github/scripts/change_versions.sh
@@ -3,8 +3,10 @@
 # Expects the following environment variables to be set:
 #   $NEW_VERSION                 (Example: "1.2.0")
 
-# There are nicer ways to do this, but this one does not require fully resolving the Maven project.
-# As such, it functions even though the SNAPSHOT referenced is not available.
+# This will fail the Maven build if the version is not available.
+# Thankfully, this is not the case (yet) in this project.
+# If/when it happens, this needs to be replaced by a manually provided version,
+# as scanning the text of the POM would be unreliable.
 DETECTED_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:3.1.0:exec)
 echo "Detected version: $DETECTED_VERSION"
 echo "     New version: $NEW_VERSION"
